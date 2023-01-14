200169f0256dcfde697dc01fc5dfc4afcda87d95
luis f sousa <rumpertumpski@gmail.com>
2023-01-14 15:29:20 -0300

    add icons to file

---
+++ src/icons.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+export const PlusIcon = (props) => (
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M6.5 6.5V2a1.5 1.5 0 0 1 3 0v4.5H14a1.5 1.5 0 0 1 0 3H9.5V14a1.5 1.5 0 0 1-3 0V9.5H2a1.5 1.5 0 0 1 0-3h4.5z"
+    />
+  </svg>
+)
