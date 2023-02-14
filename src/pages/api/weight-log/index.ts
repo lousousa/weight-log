4e10c1dfed8bcd0d3e4f80f9b15c99ac0077b4db
luis f sousa <rumpertumpski@gmail.com>
2023-02-13 21:14:01 -0300

    add api get

---
+++ src/pages/api/weight-log/index.ts
@@ -0,0 +1,44 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import createRouter from 'next-connect'
+const router = createRouter<NextApiRequest, NextApiResponse>()
+
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID)
+
+async function userServiceAccountAuth() {
+  await doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL || '',
+    private_key: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY?.replace(/\\n/g, "\n") || ''
+  })
+}
+
+async function getData() {
+  await userServiceAccountAuth()
+  await doc.loadInfo()
+  const sheet = doc.sheetsByTitle['MainSheet']
+  await sheet.resetLocalCache(true)
+  await sheet.loadCells('A1:B')
+
+  const lastRowNumber = sheet.cellStats.nonEmpty / 2
+  const data = []
+
+  for (let i = 0; i < lastRowNumber; i++) {
+    const date = sheet.getCell(i, 0)
+    const weight = sheet.getCell(i, 1)
+
+    data.push({
+      date: date.formattedValue,
+      weight: weight.formattedValue
+    })
+  }
+
+  return data
+}
+
+router.get(async (req: NextApiRequest, res: NextApiResponse<any>) => {
+  res.status(200).json(await getData())
+})
+
+export default router
+
