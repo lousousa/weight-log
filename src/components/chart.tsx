beb455bca3ceedb583c2e6e9b9ec75e6d5380533
luis f sousa <rumpertumpski@gmail.com>
2023-02-22 20:37:04 -0300

    add info subtitle

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -1,19 +1,21 @@
 import { useEffect, useRef, useState } from 'react'
 import { ILogEntry } from '@/types'
 import styled from 'styled-components'
+import moment from 'moment'
 
 interface IProps {
   data: ILogEntry[]
 }
 
-type ElementPosition = {
+type CheckpointInfo = {
   x: number,
-  y: number
+  y: number,
+  data: ILogEntry
 }
 
 export default function Chart({data}: IProps) {
   const canvas = useRef<HTMLCanvasElement | null>(null)
-  const [dotsPosition, setDotsPosition] = useState<ElementPosition[]>([])
+  const [checkpointsInfo, setCheckpointsInfo] = useState<CheckpointInfo[]>([])
 
   useEffect(() => {
     if (!canvas.current) return
@@ -35,28 +37,32 @@ export default function Chart({data}: IProps) {
     })
 
     const xStep = canvas.current.width / (data.length - 1)
-    const yStep = canvas.current.height / (maxValue - minValue)
-    const dotsPosition = []
+    const yStep = (canvas.current.height - 3) / (maxValue - minValue)
+    const checkpointsInfo = []
+
+    data = data.sort((a, b) => a.date < b.date ? -1 : 1)
 
     for (let i = 0; i < data.length - 1; i++) {
       const current = parseFloat(data[i].weight)
       const next = parseFloat(data[i + 1].weight)
 
       const x1 = i * xStep
-      const y1 = (current - minValue) * yStep
+      const y1 = (maxValue - current) * yStep
       const x2 = (i + 1) * xStep
-      const y2 = (next - minValue) * yStep
+      const y2 = (maxValue - next) * yStep
 
       ctx.moveTo(x1, y1)
       ctx.lineTo(x2, y2)
 
-      if (i === 0) dotsPosition.push({x: x1, y: y1})
+      if (i === 0) checkpointsInfo.push({x: x1, y: y1, data: data[i]})
 
-      dotsPosition.push({x: x2, y: y2})
+      checkpointsInfo.push({x: x2, y: y2, data: data[i + 1]})
     }
 
-    setDotsPosition(dotsPosition)
+    setCheckpointsInfo(checkpointsInfo)
 
+    ctx.lineWidth = 3
+    ctx.strokeStyle = '#eff2ff'
     ctx.stroke()
   }, [canvas, data])
 
@@ -64,8 +70,11 @@ export default function Chart({data}: IProps) {
     <ChartWrapper>
       <canvas ref={canvas}/>
 
-      {dotsPosition.map((dotPosition, idx) => (
-        <Dot key={idx} position={dotPosition} />
+      {checkpointsInfo.map((checkpointInfo, idx) => (
+        <Dot
+          key={idx}
+          content={checkpointInfo}
+        />
       ))}
     </ChartWrapper>
   </ChartSection>
@@ -76,17 +85,17 @@ const ChartSection = styled.div`
 `
 
 const ChartWrapper = styled.div`
-  background-color: #a2d2ff;
+  background-color: #477cff;
   padding: 16px;
   border-radius: 8px;
   position: relative;
 `
 
-const Dot = styled.div<{position: ElementPosition}>`
+const Dot = styled.div<{content: CheckpointInfo}>`
   ${props => `
     // 16 = wrapper's padding; 5 = half dot size.
-    --left: ${props.position.x + 16 - 5}px;
-    --top: ${props.position.y + 16 - 5}px;
+    --left: ${props.content.x + 16 - 5}px;
+    --top: ${props.content.y + 16 - 5}px;
   `}
 
   position: absolute;
@@ -95,12 +104,30 @@ const Dot = styled.div<{position: ElementPosition}>`
   width: 10px;
   height: 10px;
   border-radius: 50%;
-  background-color: #003566;
+  background-color: #eff2ff;
   cursor: pointer;
   box-sizing: content-box;
 
   &:hover {
-    border: 4px solid #fff;
+    border: 4px solid #222;
     margin: -4px 0 0 -4px;
+
+    &::before {
+      ${props => `
+        content: '${props.content.data.weight} -
+          ${moment(props.content.data.date).format('ll')}';
+      `}
+
+      position: absolute;
+      top: -40px;
+      left: -55px;
+      width: 110px;
+      padding: 8px;
+      z-index: 2;
+      background: linear-gradient(to top, #222, #444);
+      color: #ddd;
+      border-radius: 4px;
+      font-size: 12px;
+    }
   }
 `
